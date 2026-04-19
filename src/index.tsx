b8b260c5219852044722e92512e7746862614b72
Yuri Sulyma <yuri.sulyma@protonmail.com>
2026-04-19 00:15:51 -0600

    update typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement!);
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
 
 root.render(
 	<StrictMode>
